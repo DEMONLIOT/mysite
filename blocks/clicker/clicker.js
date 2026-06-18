bfcc6bb79ef595f0714b570faa9b5589bfe562a7
DEMONLIOT <ekautzmann@ent-saint-denis.net>
2026-06-18 16:21:37 +0200

    Update score handling and display in clicker.js

--- blocks/clicker/clicker.js
+++ blocks/clicker/clicker.js
@@ -1,11 +1,16 @@
 export default function decorate(block) {
   // --- 1. TES COMPTEURS (Les variables de stockage) ---
+  const scoreSauvegarde=localStorage.getItem('clickerScore')
   let score = 0;
+if (scoreSauvegarde) {
+  score=parseInt(scoreSauvegarde,10);
+  }else let score=0{
+  }
   let pointsParClic = 1;
 
   // --- 2. L'AFFICHAGE (Ce que le joueur voit à l'écran) ---
   const scoreDisplay = document.createElement('h2');
-  scoreDisplay.textContent = "Score : 0";
+  scoreDisplay.textContent = "Score :"+score;
   scoreDisplay.style.fontSize = "2rem";
 
   const clickButton = document.createElement('button');
@@ -24,6 +29,6 @@ export default function decorate(block) {
     // ÉTAPE 1 : Écris la ligne pour augmenter le score
     scoreDisplay.textContent="Score:"+score
     // ÉTAPE 2 : Écris la ligne pour mettre à jour le texte affiché à l'écran
-
+localStorage.setItem('clickerScore', score);
   });
 }
