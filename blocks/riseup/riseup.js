b4a1b55fd962883cc562d87cf46b5943f2db1637
DEMONLIOT <ekautzmann@ent-saint-denis.net>
2026-06-18 09:58:59 +0200

    Clean up comments in riseup.js
    Removed comments related to initial state and event listener.

--- blocks/riseup/riseup.js
+++ blocks/riseup/riseup.js
@@ -1,12 +1,9 @@
 export default function decorate(block) {
-  // Masqué au départ
   block.style.opacity = '0';
   block.style.transform = 'translateY(60px)';
   block.style.transition = 'transform 2s cubic-bezier(0.25, 1, 0.5, 1), opacity 2s';
 
-  // Écouter le signal de fin de la tempête de neige
   document.addEventListener('textHasFallen', () => {
-    // Le texte monte à sa place
     block.style.opacity = '1';
     block.style.transform = 'translateY(0)';
   });
