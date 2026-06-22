f6256e270cbbdc4ce7ae03614036f6fb33b7bbf7
DEMONLIOT <ekautzmann@ent-saint-denis.net>
2026-06-22 11:19:28 +0200

    Enhance header decoration with navigation fragment
    The function now loads a navigation fragment and decorates the header block with it.

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,3 +1,21 @@
-export default function decorate() {
-  // On laisse vide, scripts.js gère tout de manière autonome !
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
+
+/**
+ * decorates the header, loads the header fragment
+ * @param {Element} block The header block
+ */
+export default async function decorate(block) {
+  // load nav as fragment
+  const navMeta = getMetadata('nav');
+  const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
+  const fragment = await loadFragment(navPath);
+
+  // decorate nav DOM
+  block.textContent = '';
+  const nav = document.createElement('nav');
+  nav.id = 'nav';
+  while (fragment.firstElementChild) nav.append(fragment.firstElementChild);
+
+  block.append(nav);
 }
